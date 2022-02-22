6fc30f730b4ff6a48424db61ec4d6bb90562fc72
ViroMist <pabg1928@yandex.ru>
2022-02-23 00:36:37 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace RougeLike
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this._knight = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this._knight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _knight
+            // 
+            this._knight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this._knight.Location = new System.Drawing.Point(104, 300);
+            this._knight.Name = "_knight";
+            this._knight.Size = new System.Drawing.Size(20, 40);
+            this._knight.TabIndex = 0;
+            this._knight.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._knight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this._knight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox _knight;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RougeLike
+{
+    public partial class Form1 : Form
+    {
+        int _point = 0;
+        private PictureBox[] enemy = new PictureBox[400];
+        private int[] enemyHP = new int[400];
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(WASD);
+            //враг
+            enemy[0] = new PictureBox();
+            enemy[0].BackColor = Color.Gray;
+            enemy[0].Size = new Size(20, 40);
+            enemy[0].Location = new Point(100, 100);
+            enemyHP[0] = 2;
+            //отоброжение врага
+            this.Controls.Add(enemy[0]);
+            //таймер
+            timer.Tick += new EventHandler(_update);
+            timer.Interval = 1;
+            timer.Start();
+        }
+        
+        //движение врага
+        public void _update(Object ObjectEnemy, EventArgs eventAgr)
+        {
+            for (int i = _point; i >= 0; --i)
+            {
+                // Следование
+                if(enemyHP[i] == 2)
+                {
+                    // X
+                    if (enemy[i].Location.X > _knight.Location.X)
+                    {
+                        enemy[i].Location = new Point(enemy[i].Location.X - 2, enemy[i].Location.Y);
+                    }
+                    else if (enemy[i].Location.X < _knight.Location.X)
+                    {
+                        enemy[i].Location = new Point(enemy[i].Location.X + 2, enemy[i].Location.Y);
+                    }
+                    // Y
+                    if (enemy[i].Location.Y > _knight.Location.Y)
+                    {
+                        enemy[i].Location = new Point(enemy[i].Location.X, enemy[i].Location.Y - 2);
+                    }
+                    else if (enemy[i].Location.Y < _knight.Location.Y)
+                    {
+                        enemy[i].Location = new Point(enemy[i].Location.X, enemy[i].Location.Y + 2);
+                    }
+                }
+                // Побег
+                if (enemyHP[i] == 1)
+                {
+                    // X
+                    if (enemy[i].Location.X > _knight.Location.X)
+                    {
+                        enemy[i].Location = new Point(enemy[i].Location.X + 1, enemy[i].Location.Y);
+                    }
+                    else if (enemy[i].Location.X < _knight.Location.X)
+                    {
+                        enemy[i].Location = new Point(enemy[i].Location.X - 1, enemy[i].Location.Y);
+                    }
+                    // Y
+                    if (enemy[i].Location.Y > _knight.Location.Y)
+                    {
+                        enemy[i].Location = new Point(enemy[i].Location.X, enemy[i].Location.Y + 1);
+                    }
+                    else if (enemy[i].Location.Y < _knight.Location.Y)
+                    {
+                        enemy[i].Location = new Point(enemy[i].Location.X, enemy[i].Location.Y - 1);
+                    }
+                }
+            }
+        }
+        //движение персонажа
+        private void WASD(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "D":
+                    _knight.Location = new Point(_knight.Location.X + 50, _knight.Location.Y);
+                    break;
+                case "A":
+                    _knight.Location = new Point(_knight.Location.X - 50, _knight.Location.Y);
+                    break;
+                case "W":
+                    _knight.Location = new Point(_knight.Location.X, _knight.Location.Y - 50);
+                    break;
+                case "S":
+                    _knight.Location = new Point(_knight.Location.X, _knight.Location.Y + 50);
+                    break;
+            }
+        }
+    }
+}
